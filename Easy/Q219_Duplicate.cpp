8e991e0a56fcaae61edaa2f4e212af3a9351fd32
Hitesh Bidawatka <bidawatkahitesh4@gmail.com>
2026-07-04 16:01:14 +0530

    Q219

---
+++ Easy/Q219_Duplicate.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+bool ContainNearByDuplicate(vector<int> &nums , int k){
+
+    unordered_set<int> window;
+
+    for(int i=0; i<nums.size(); i++){
+        if(window.find(nums[i]) != window.end()){
+            return true;
+        }
+
+        window.insert(nums[i]);
+
+        if(window.size() > k){
+            window.erase(nums[i-k]);
+        }
+    }
+
+    return false;
+}
+
+int main(){
+
+    vector<int> nums = {1 , 2 , 3 , 1};
+    int k = 3;
+
+    cout << ContainNearByDuplicate(nums , k);
+
+    return 0;
+}
