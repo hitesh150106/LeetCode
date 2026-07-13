d8a4065f290fbbdec0fa403cacc54aafe8d15184
Hitesh Bidawatka <bidawatkahitesh4@gmail.com>
2026-07-13 12:47:30 +0530

    Q443

---
+++ Medium/Q443_StringCompression.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// TC : O(n)
+int StringCompression(vector<char>& chars){
+    
+    int n = chars.size();
+    int idx = 0;
+
+    for(int i=0; i<n; i++){
+        int count = 0;
+        char ch = chars[i];
+
+        while(i < n && chars[i] == ch){
+            count++;
+            i++;
+        }
+
+        if(count == 1){
+            chars[idx++] = ch;
+        } else{
+            chars[idx++] = ch;
+            string str = to_string(count);
+            for(char dig : str){
+                chars[idx++] = dig;
+            }
+        }
+        i--;
+    }
+    chars.resize(idx);
+    return idx;
+}
+
+int main(){
+
+    vector<char> chars = {'a','a','b','b','c','c','c'};
+
+    cout << StringCompression(chars);
+
+    return 0;
+}
