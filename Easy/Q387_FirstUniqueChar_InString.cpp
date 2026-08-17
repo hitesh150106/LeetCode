5fda54531bfa81b73671a13e97dbd0d37ddf9a2b
Hitesh Bidawatka <bidawatkahitesh4@gmail.com>
2026-08-17 14:28:33 +0530

    Q387-U

--- Easy/Q387_FirstUniqueChar_InString.cpp
+++ Easy/Q387_FirstUniqueChar_InString.cpp
@@ -17,12 +17,33 @@ int uniqueString(string s){
     return -1;
 }
 
+// More Optimal
+int uniqueString2(string s){
+
+    int count[26] = {0};
+
+    for(char c : s){
+        count[c-'a']++;
+    }
+
+    for(int i=0; i<s.length(); i++){
+        if(count[s[i]-'a'] == 1){
+            return i;
+        }
+    }
+
+    return -1;
+}
+
 int main(){
 
     string s = "aabb";
 
     cout << "My Approach : ";
     cout << uniqueString(s) << endl;
+
+    cout << "Another Approach : ";
+    cout << uniqueString2(s);    
 
     return 0;
 }
