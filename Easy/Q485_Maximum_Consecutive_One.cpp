30539f1c657a7a9798ea3aa7088342da99afd3fd
Hitesh Bidawatka <bidawatkahitesh4@gmail.com>
2026-08-26 14:05:08 +0530

    Q485

---
+++ Easy/Q485_Maximum_Consecutive_One.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int maxConsecutive(vector<int> nums){
+
+    int maxCount = 0;
+    int count = 0;
+
+    for(int i=0; i<nums.size(); i++){
+        if(nums[i] == 1){
+            count++;
+            maxCount = max(maxCount , count);
+        } else {
+            count = 0;
+        }
+    }
+
+    return maxCount;
+}
+
+int main(){
+
+    vector<int> nums = {1 , 0 , 1 , 1 , 0 , 1};
+
+    cout << maxConsecutive(nums);
+
+    return 0;
+}
